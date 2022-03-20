4bcea4e8aa365301df3f8affac646fe01381c984
Dylan Cheng <newlooper@hotmail.com>
2022-03-20 09:25:02 +0800

    Cude3D may render in software by check RenderCapability.Tier.

--- Plugins.sln/Cube3D/FrameToD3DImage.cs
+++ Plugins.sln/Cube3D/FrameToD3DImage.cs
@@ -11,8 +11,10 @@ You should have received a copy of the GNU General Public License along with Vir
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Windows;
 using System.Windows.Interop;
+using System.Windows.Media;
 using ScreenCapture;
 
 namespace Cube3D
@@ -20,7 +22,9 @@ namespace Cube3D
     public class FrameToD3DImage : FrameProcessor
     {
         private readonly Dictionary<string, D3DImageInfo> _d3DImageDict;
-        private readonly Duration                         _duration = new( new TimeSpan( 0, 0, 0, 0, 16 ) );
+        private readonly Duration                         _duration          = new( new TimeSpan( 0, 0, 0, 0, 16 ) );
+        private readonly TimeSpan                         _forceDrawInterval = new( 0, 0, 10, 0 );
+        private readonly Stopwatch                        _sw                = Stopwatch.StartNew();
 
         private FrameToD3DImage()
         {
@@ -47,19 +51,49 @@ namespace Cube3D
 
         public override void Proceed( IntPtr pointer )
         {
-            foreach ( var (_, value) in _d3DImageDict )
+            foreach ( var (type, value) in _d3DImageDict )
             {
-                if ( !value.Draw ) continue;
-                var d3DImage = value.Image;
-
-                if ( d3DImage.IsFrontBufferAvailable && d3DImage.TryLock( _duration ) )
+                switch ( type )
                 {
-                    d3DImage.SetBackBuffer( D3DResourceType.IDirect3DSurface9, pointer );
-                    d3DImage.AddDirtyRect( new Int32Rect( 0, 0, d3DImage.PixelWidth, d3DImage.PixelHeight ) );
-                }
+                    case D3DImages.Front when value.Draw:
+                        Paint( value, pointer );
+                        break;
+                    case D3DImages.Others when value.Draw:
+                        Paint( value, pointer );
+                        break;
+                    case D3DImages.Others when !value.Draw:
+                        if ( _sw.Elapsed > _forceDrawInterval )
+                        {
+                            Paint( value, pointer );
+                            _sw.Restart();
+                        }
 
-                d3DImage.Unlock();
+                        break;
+                }
             }
+        }
+
+        private static bool IsSoftRender()
+        {
+            var level = RenderCapability.Tier >> 16;
+            return level == 0;
+        }
+
+        private void Paint( D3DImageInfo dii, IntPtr pointer )
+        {
+            var useSoftRender = IsSoftRender();
+
+            var d3DImage = dii.Image;
+
+            if ( !useSoftRender && !d3DImage.IsFrontBufferAvailable ) return;
+
+            if ( d3DImage.TryLock( _duration ) )
+            {
+                d3DImage.SetBackBuffer( D3DResourceType.IDirect3DSurface9, pointer, useSoftRender );
+                d3DImage.AddDirtyRect( new Int32Rect( 0, 0, d3DImage.PixelWidth, d3DImage.PixelHeight ) );
+            }
+
+            d3DImage.Unlock();
         }
     }
 
