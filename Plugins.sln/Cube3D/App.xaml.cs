ac182fa9fdac8d79cc5ec1938c52470600421d83
Dylan Cheng <newlooper@hotmail.com>
2022-04-01 11:13:37 +0800

    [assembly.property] Cube3D auto versioned on build.

--- Plugins.sln/Cube3D/App.xaml.cs
+++ Plugins.sln/Cube3D/App.xaml.cs
@@ -1,12 +1,12 @@
 ﻿/* Copyright (C) 2022 Dylan Cheng (https://github.com/newlooper)
 
-This file is part of VirtualSpace.
+This file is part of Cube3D.
 
-VirtualSpace is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+Cube3D is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
 
-VirtualSpace is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+Cube3D is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License along with VirtualSpace. If not, see <https://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License along with Cube3D. If not, see <https://www.gnu.org/licenses/>.
 */
 
 using System.Windows;
